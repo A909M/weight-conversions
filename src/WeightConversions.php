f8f597ec94c7d4ce4223cb647be54bbd73c7536e
A909M <assemcc2022@gmail.com>
2025-01-06 20:01:06 +0300

    add Conversion

--- src/WeightConversions.php
+++ src/WeightConversions.php
@@ -2,4 +2,16 @@
 
 namespace A909M\WeightConversions;
 
-class WeightConversions {}
+class WeightConversions
+{
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+    public function __construct(protected float $kilograms) {}
+
+    public function toPounds(): float
+    {
+        return $this->kilograms * 2.20462;
+    }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,12 @@
 <?php
 
+use A909M\WeightConversions\WeightConversions;
+
 it('can test', function () {
     expect(true)->toBeTrue();
+});
+
+it('can convert kilograms to pounds', function () {
+    $pounds = WeightConversions::kilograms(100)->toPounds();
+    expect($pounds)->toBe(220.462);
 });
